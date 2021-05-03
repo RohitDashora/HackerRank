5db15f4cc0c73218e2ce8a4754ed14ab6e6c2dc3
Rohit Dashora <rohitdashora@gmail.com>
2021-05-03 18:55:30 +0530

    added more code

--- algoritms/VaccinationSpots.py
+++ algoritms/VaccinationSpots.py
@@ -1,31 +1,55 @@
 import requests
 import json
 import datetime
+from datetime import date
+from datetime import timedelta
 
-#def __main__():
-endpoint = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=313001&date=03-05-2021'
+endpoint = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?'
+ageLimit=45 #chnage it to 18
 pincode='313001'
-date='03-05-2021'
-url=endpoint+'pincode='+pincode+'&date='+date
-print(url)
-response=requests.get(url)
-vacdata=response.json()
+#date='03-05-2021'
+
+def pollURL(endpoint, pincode, date):
+    url=endpoint+'pincode='+pincode+'&date='+date
+    #print(url)
+    response=requests.get(url)
+    return(response)
 
 
-center_count =len(vacdata["centers"])
+if __name__ == '__main__':
+    
+    dates=[]
+    for i in range(0,8):
+        d= date.today()+timedelta(days=i) 
+        dates.append(d.strftime("%d-%m-%Y"))
 
+    #lets get data for next 1 week
 
-for i in range(0,center_count):
-    sessions = vacdata["centers"][i]["sessions"]
-    location = vacdata["centers"][i]["name"]
-    freepaid =vacdata["centers"][i]["fee_type"]
-    available=0
-    for i in range(len(sessions)):
-        min_age_limit =sessions[i]["min_age_limit"]
-        available=sessions[i]["available_capacity"]
-        slot= sessions[i]["slots"]
-        if min_age_limit == 18 & available > 0 :
-            print(location,freepaid, slot,available)
+    slot_list =[]
+    for date in dates :
+        response = pollURL(endpoint, pincode, date)    
+        vacdata=response.json()
+        #print(vacdata)
+        center_count =len(vacdata["centers"])
+        for i in range(0,center_count):
+            sessions = vacdata["centers"][i]["sessions"]
+            location = vacdata["centers"][i]["name"]
+            freepaid =vacdata["centers"][i]["fee_type"]
+           
+            for i in range(len(sessions)):
+                session_list=[]
+                min_age_limit =sessions[i]["min_age_limit"]
+                available=sessions[i]["available_capacity"]
+                slot= sessions[i]["slots"]
+                if min_age_limit == ageLimit and available > 0 :
+                    session_list.append(sessions[i]["date"])
+                    session_list.append(location)
+                    session_list.append(freepaid)
+                    session_list.append(sessions[i]["vaccine"])
+                    session_list.append(slot)
+                    session_list.append(available)
+                    slot_list.append(session_list)
+    print(*slot_list, sep='\n')
 
 
 
